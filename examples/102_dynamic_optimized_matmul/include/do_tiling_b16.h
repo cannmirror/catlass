80d22ddb390dc953b9d0210f13a6afce9575c914
weixin_42818618 <zhusongxiang1@huawei.com>
2025-10-15 17:19:53 +0800

    更新tiling文件

--- examples/102_dynamic_optimized_matmul/include/do_tiling_b16.h
+++ examples/102_dynamic_optimized_matmul/include/do_tiling_b16.h
@@ -14,8 +14,7 @@
 #include "utils.h"
 #include "tiling_params.h"
 #include "platform_info.h"
-#include <opdev/bfloat16.h>
-#include <opdev/fp16_t.h>
+#include "fp16_t.h"
 
 using fp16_t = op::fp16_t;
 
@@ -27,7 +26,7 @@ void DoTilingB16Layout00(TilingParams &tilingParams, PlatformInfo& platformInfo)
     uint32_t m1 = 128, n1 = 256, k1 = 256;
 
     if (n >= 256) {
-        // n1 = 256 delivers optimal bandwidth performance.
+        // n0 = 256 delivers optimal bandwidth performance.
         uint32_t maxBlocks = RoundUp(CeilDiv(m, m1) * CeilDiv(n, n1), platformInfo.coreNum);
         BalanceWorkload(m, n, m1, n1, 32, platformInfo);
         uint32_t blocks = CeilDiv(m, 64) * CeilDiv(n, 512);
@@ -38,7 +37,6 @@ void DoTilingB16Layout00(TilingParams &tilingParams, PlatformInfo& platformInfo)
     } else {
         m1 = 128;
         n1 = RoundUp(n, 16);
-        BalanceWorkload(m, n, m1, n1, 32, platformInfo);
         uint32_t maxBlocks = RoundUp(CeilDiv(m, m1) * CeilDiv(n, n1), platformInfo.coreNum);
         uint32_t m1t = m1;
         while (JudgeSpace<fp16_t>(m1t + 16, n1, k1, platformInfo)) {
@@ -48,8 +46,9 @@ void DoTilingB16Layout00(TilingParams &tilingParams, PlatformInfo& platformInfo)
                 m1 = m1t;
             }
         }
+        BalanceWorkload(m, n, m1, n1, 32, platformInfo);
     }
-    if (k >= 65536 || n > 65536) {
+    if (k >= 65536 || n >= 65536) {
         m1 = 128;
         n1 = 256;
     }
@@ -85,6 +84,8 @@ void DoTilingB16Layout01(TilingParams &tilingParams, PlatformInfo& platformInfo)
                 n1 = n1t;
             }
         }
+        BalanceWorkload(m, n, m1, n1, 64, platformInfo);
+        BalanceWorkload(n, m, n1, m1, 64, platformInfo); 
     } else {
         uint32_t m1t = m1;
         while (JudgeSpace<fp16_t>(m1t + 16, n1, k1, platformInfo)) {
@@ -94,8 +95,10 @@ void DoTilingB16Layout01(TilingParams &tilingParams, PlatformInfo& platformInfo)
                 m1 = m1t;
             }
         }
+        BalanceWorkload(n, m, n1, m1, 64, platformInfo);
+        BalanceWorkload(m, n, m1, n1, 64, platformInfo); 
     }
-    if (k > 65536) {
+    if (k >= 65536) {
         if (m < n || (ratio < 0.1 && n >= 256)) {
             m1 = 128;
             n1 = 256;
@@ -151,8 +154,8 @@ void DoTilingB16Layout10(TilingParams &tilingParams, PlatformInfo& platformInfo)
             n1 = 128;
         }
     }
-    m1 = RoundUp(m1, 16);
-    n1 = RoundUp(n1, 16);
+    m1 = m1 / 16 * 16;
+    n1 = n1 / 16 * 16;
     k1 = GetMaxK1<fp16_t>(m1, n1, platformInfo);
     SetTile(tilingParams, m1, n1, k1); 
 }
@@ -165,7 +168,7 @@ void DoTilingB16Layout11(TilingParams &tilingParams, PlatformInfo& platformInfo)
     uint32_t m1 = 256, n1 = 128, k1 = 256;
 
     if (m >= 256) {
-        // m1 = 256 delivers optimal bandwidth performance.
+        // n0 = 256 delivers optimal bandwidth performance.
         uint32_t maxBlocks = RoundUp(CeilDiv(m, m1) * CeilDiv(n, n1), platformInfo.coreNum);
         BalanceWorkload(n, m, n1, m1, 32, platformInfo);
         uint32_t blocks = CeilDiv(n, 64) * CeilDiv(m, 512);
@@ -176,7 +179,6 @@ void DoTilingB16Layout11(TilingParams &tilingParams, PlatformInfo& platformInfo)
     } else {
         n1 = 128;
         m1 = RoundUp(m, 16);
-        BalanceWorkload(n, m, n1, m1, 32, platformInfo);
         uint32_t maxBlocks = RoundUp(CeilDiv(m, m1) * CeilDiv(n, n1), platformInfo.coreNum);
         uint32_t n1t = n1;
         while (JudgeSpace<fp16_t>(n1t + 16, m1, k1, platformInfo)) {
@@ -186,8 +188,9 @@ void DoTilingB16Layout11(TilingParams &tilingParams, PlatformInfo& platformInfo)
                 n1 = n1t;
             }
         }
+        BalanceWorkload(n, m, n1, m1, 32, platformInfo);
     }
-    if (k >= 65536 || m > 65536) {
+    if (k >= 65536 || m >= 65536) {
         m1 = 256;
         n1 = 128;
     }
