ee519356202beffa6e1d89a22e3d2674028799e2
zhanghaobo <zhanghaobo6@huawei.com>
2025-10-21 20:35:47 +0800

    FP8 PROLOGUE MODIFICATION
    Keep tile_copy.hpp
    
    Still cerr
    
    Bugfix#1 neccessary header files & wrong typo error
    
    Update block-level computing
    
    Bugfix#2 bufferIndex & bufferIndexforCast
    
    Adjust prologue lifetime as it before
    
    bugfix#5
    
    move wks for better presentation
    
    remove comments
    
    delete old block level file
    
    remove comments#2
    
    remove unccessary files

--- include/catlass/gemm/dispatch_policy.hpp
+++ include/catlass/gemm/dispatch_policy.hpp
@@ -35,13 +35,13 @@ struct MmadAtlasA2Pingpong : public MmadAtlasA2  {
 };
 
 template <bool ENABLE_UNIT_FLAG_ = false>
-struct MmadAtlasA2PingpongSliceK : public MmadAtlasA2  {
+struct MmadAtlasA2PingPongWithPrologue : public MmadAtlasA2 {
     static constexpr uint32_t STAGES = 2;
     static constexpr bool ENABLE_UNIT_FLAG = ENABLE_UNIT_FLAG_;
 };
 
 template <bool ENABLE_UNIT_FLAG_ = false>
-struct MmadAtlasA2PingPongWithPrologue : public MmadAtlasA2 {
+struct MmadAtlasA2PingpongSliceKWithPrologue : public MmadAtlasA2 {
     static constexpr uint32_t STAGES = 2;
     static constexpr bool ENABLE_UNIT_FLAG = ENABLE_UNIT_FLAG_;
 };
