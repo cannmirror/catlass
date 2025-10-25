9a7d0da7244e833429d78d694844ae5899bf88c7
zhanghaobo <zhanghaobo6@huawei.com>
2025-10-25 09:58:37 +0800

    Bugfix#2 remove int8fp16 header

--- include/catlass/gemm/tile/tile_copy.hpp
+++ include/catlass/gemm/tile/tile_copy.hpp
@@ -22,7 +22,7 @@
 #include "catlass/gemm/tile/copy_l1_to_bt.hpp"
 #include "catlass/gemm/tile/copy_gm_to_ub.hpp"
 #include "catlass/gemm/tile/copy_ub_to_gm.hpp"
-#include "catlass/gemm/tile/cast_int8_to_fp16.hpp"
+// #include "catlass/gemm/tile/cast_int8_to_fp16.hpp"
 #include "catlass/gemm/tile/cast_fp8_to_fp16.hpp"
 #include "catlass/gemm/helper.hpp"
 
