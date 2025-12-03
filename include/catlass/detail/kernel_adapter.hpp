2c3d10f066fe0c067d81520873da3f79dae09401
yuantao <taoyuan15@h-partners.com>
2025-12-03 17:51:07 +0800

    device layer

--- include/catlass/detail/kernel_adapter.hpp
+++ include/catlass/detail/kernel_adapter.hpp
@@ -4,53 +4,29 @@
  * This file is a part of the CANN Open Software.
  * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. See LICENSE in the root of
+ * the software repository for the full text of the License.
  */
- 
+
 #ifndef CATLASS_DETAIL_KERNEL_ADAPTER_HPP
 #define CATLASS_DETAIL_KERNEL_ADAPTER_HPP
 
 #include "catlass/catlass.hpp"
 
-#if defined(ENABLE_ASCENDC_DUMP)
-#include "catlass/debug.hpp"
-namespace Catlass {
-/// Generic Catlass kernel template
-template <class Operator>
-CATLASS_GLOBAL void KernelAdapter(typename Operator::Params params, GM_ADDR ptrDump)
-{
-    Operator op;
-    AscendC::InitDump(false, ptrDump, ALL_DUMPSIZE);
-    op(params);
-}
+#ifndef KERNEL_TASK_TYPE
+#define KERNEL_TASK_TYPE KERNEL_TYPE_MIX_AIC_1_2
+#endif
 
-template <class Operator>
-CATLASS_GLOBAL void KernelAdapter(typename Operator::Params params, uint64_t fftsAddr, GM_ADDR ptrDump)
-{
-    AscendC::SetSyncBaseAddr(fftsAddr);
-    Operator op;
-    AscendC::InitDump(false, ptrDump, ALL_DUMPSIZE);
-    op(params);
-}
-} // namespace Catlass
-#else
 namespace Catlass {
 /// Generic Catlass kernel template
 template <class Operator>
 CATLASS_GLOBAL void KernelAdapter(typename Operator::Params params)
 {
+    KERNEL_TASK_TYPE_DEFAULT(KERNEL_TASK_TYPE);
     Operator op;
     op(params);
 }
 
-template <class Operator>
-CATLASS_GLOBAL void KernelAdapter(typename Operator::Params params, uint64_t fftsAddr)
-{
-    AscendC::SetSyncBaseAddr(fftsAddr);
-    Operator op;
-    op(params);
-}
 } // namespace Catlass
-#endif
 #endif
